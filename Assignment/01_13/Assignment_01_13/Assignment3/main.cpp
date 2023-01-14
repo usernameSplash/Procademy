5d8365e62f538c02e32ad78cacbcd6cfe490a0d0
Kyeongmin Kim <kkfenix3085@naver.com>
2023-01-14 21:05:22 +0900

    Solve Assignment_01_13 - 3

---
+++ Assignment/01_13/Assignment_01_13/Assignment3/main.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include <iostream>
+
+void ControlEachByteOfNumber()
+{
+	uint32_t number;
+	number = 0;
+
+	while (true)
+	{
+		uint8_t bytePosition;
+		uint8_t tempValue;
+		uint32_t value;
+
+		printf("Byte Position (1~4): ");
+		scanf("%hhu", &bytePosition);
+
+		printf("Value (0~255): ");
+		scanf("%hhu", &tempValue);
+
+		value = tempValue << ((bytePosition - 1) * 8);				// Shift Input Value to Correct Byte Zone.
+		value = ~(0) & value;									// Make All Other Bits <1> for Change Value on Only Specific Byte Zone.
+
+		number = number & ~(0xFF << ((bytePosition - 1) * 8));	// Erase Bits of `number` on Specific Byte Zone.
+		number = number | value;								// Apply Input Value on Specific Byte Zone.
+
+		for (int i = 0; i < 4; i++)
+		{
+			printf("Byte Zone %d: %d\n", (i + 1), (uint8_t)(number >> (i * 8)));
+		}
+
+		printf("Total 4 Byte Integer Value: %#010x\n\n", number);
+	}
+}
+int main()
+{
+	ControlEachByteOfNumber();
+	return 0;
+}
