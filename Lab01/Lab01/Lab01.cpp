6cbb01a56b0b28900359d34410a25a2192d5e127
Kyeongmin Kim <kkfenix3085@naver.com>
2023-01-14 16:40:15 +0900

    Initial Commit

---
+++ Lab01/Lab01/Lab01.cpp
@@ -0,0 +1,22 @@
+﻿// Lab01.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+void Test(void)
+{
+    int a;
+    int b;
+    int c;
+
+    a = 0xff;
+    b = 0xffff;
+
+    return;
+}
+
+int main()
+{
+    Test();
+    return 0;
+}
