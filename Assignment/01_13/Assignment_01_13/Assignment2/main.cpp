40dc788141ef3acf766f8c900e21142d85f9a2cd
Kyeongmin Kim <kkfenix3085@naver.com>
2023-01-14 19:43:23 +0900

    Solve Assignment_01_13 - 2

--- Assignment/01_13/Assignment_01_13/Assignment2/main.cpp
+++ Assignment/01_13/Assignment_01_13/Assignment2/main.cpp
@@ -1,10 +1,48 @@
-#include <cstdio>
 #include <cstddef>
 #include <iostream>
+#include <iomanip>
 
+void ControlEachBitOfNumber()
+{
+	uint16_t number;
+	number = 0;
+	
+	while (true) {
+		int bitPosition;
+		int bitStatus;
 
+		std::cout << "비트 위치: ";
+		std::cin >> bitPosition;
+
+		std::cout << "OFF/ON [0,1]: ";
+		std::cin >> bitStatus;
+
+		if (bitPosition < 0 || bitPosition > 16)
+		{
+			std::cout << "비트 범위를 초과하였습니다." << "\n";
+			continue;
+		}
+
+		if(bitStatus) 
+		{
+			number = number | (1 << (bitPosition - 1));
+		}
+		else
+		{
+			number = number & ~(1 << (bitPosition - 1));
+		}
+
+		for (int pos = 15; pos >= 0; pos--)
+		{
+			std::cout << std::setw(2) << (pos + 1) << "번 Bit: " << (bool)(number & (1 << pos)) << "\n";
+		}
+
+		std::cout << "비트 패턴 해석 결과: " << number << "\n" << std::endl;
+	}
+}
 
 int main()
 {
+	ControlEachBitOfNumber();
 	return 0;
 }
