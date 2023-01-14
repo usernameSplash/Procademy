a3d16ae588d3d8d1dd3d554d78c18f33f5617d1b
Kyeongmin Kim <kkfenix3085@naver.com>
2023-01-14 19:02:03 +0900

    Solve Assignment_01_13 - 1

---
+++ Assignment/01_13/Assignment_01_13/Assignment_01_13/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <cstdio>
+#include <cstddef>
+#include <iostream>
+
+void PrintEachBitOfNumber()
+{
+	uint8_t number;
+	std::cout << "Input Number: ";
+	scanf("%hhu", &number);
+
+	uint8_t curBit = 1 << 7; //MSB of uint8
+
+	printf("%d의 바이너리: ", number);
+
+	while (curBit != 0)
+	{
+		if (curBit & number) {
+			printf("%d", 1);
+		}
+		else {
+			printf("%d", 0);
+		}
+
+		curBit = curBit >> 1;
+	}
+
+	return;
+}
+
+int main()
+{
+	PrintEachBitOfNumber();
+}
