0ad532c5a791cc46ff73cfcf1f6e3aa7755ca7fd
Kyeongmin Kim <kkfenix3085@naver.com>
2023-02-20 04:03:55 +0900

    Project_02_13 Split Rendering Part

---
+++ Project/Project_02_13/Project_02_13/Render.cpp
@@ -0,0 +1,53 @@
+#include "Console.h"
+#include "Render.h"
+
+#include <cstdio>
+#include <Windows.h>
+
+//--------------------------------------------------------------------
+// 버퍼의 내용을 화면으로 찍어주는 함수.
+//
+// 적군,아군,총알 등을 szScreenBuffer 에 넣어주고, 
+// 1 프레임이 끝나는 마지막에 본 함수를 호출하여 버퍼 -> 화면 으로 그린다.
+//--------------------------------------------------------------------
+void Buffer_Flip(void)
+{
+	int iCnt;
+	for (iCnt = 0; iCnt < dfSCREEN_HEIGHT; iCnt++)
+	{
+		cs_MoveCursor(0, iCnt);
+		printf(szScreenBuffer[iCnt]);
+	}
+}
+
+
+//--------------------------------------------------------------------
+// 화면 버퍼를 지워주는 함수
+//
+// 매 프레임 그림을 그리기 직전에 버퍼를 지워 준다. 
+// 안그러면 이전 프레임의 잔상이 남으니까
+//--------------------------------------------------------------------
+void Buffer_Clear(void)
+{
+	int iCnt;
+	memset(szScreenBuffer, ' ', dfSCREEN_WIDTH * dfSCREEN_HEIGHT);
+
+	for (iCnt = 0; iCnt < dfSCREEN_HEIGHT; iCnt++)
+	{
+		szScreenBuffer[iCnt][dfSCREEN_WIDTH - 1] = '\0';
+	}
+
+}
+
+//--------------------------------------------------------------------
+// 버퍼의 특정 위치에 원하는 문자를 출력.
+//
+// 입력 받은 X,Y 좌표에 아스키코드 하나를 출력한다. (버퍼에 그림)
+//--------------------------------------------------------------------
+void Sprite_Draw(int iX, int iY, char chSprite)
+{
+	if (iX < 0 || iY < 0 || iX >= dfSCREEN_WIDTH - 1 || iY >= dfSCREEN_HEIGHT)
+		return;
+
+	szScreenBuffer[iY][iX] = chSprite;
+}
