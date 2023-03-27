36d68dc9fd50cc9aa4bad501dc46cd14e3de503d
Kyeongmin Kim <kkfenix3085@naver.com>
2023-03-27 17:58:05 +0900

    Assignment_03_06 Linked List Complete

--- Assignment/03_06/Assginment_03_06/Assginment_03_06/List.h
+++ Assignment/03_06/Assginment_03_06/Assginment_03_06/List.h
@@ -2,7 +2,7 @@
 
 namespace MyDataStructure
 {
-	template<typename T>
+	template <typename T>
 	class List
 	{
 	private:
@@ -24,7 +24,7 @@ namespace MyDataStructure
 				mNode = node;
 			}
 
-			iterator operator ++(int)
+			iterator operator++(int)
 			{
 				iterator temp = *this;
 				mNode = mNode->mNext;
@@ -33,11 +33,11 @@ namespace MyDataStructure
 
 			iterator& operator++()
 			{
-				mNode->mNext;
+				mNode = mNode->mNext;
 				return *this;
 			}
 
-			iterator operator --(int)
+			iterator operator--(int)
 			{
 				iterator temp = *this;
 				mNode = mNode->mPrev;
@@ -46,18 +46,23 @@ namespace MyDataStructure
 
 			iterator& operator--()
 			{
-				mNode->mPrev;
+				mNode = mNode->mPrev;
 				return *this;
 			}
 
-			T& operator *()
+			T& operator*()
 			{
 				return mNode->mData;
 			}
 
-			bool operator ==(const iterator& other)
+			T& operator->()
 			{
-				if (mNode->mData == other->mNode)
+				return mNode->mData;
+			}
+
+			bool operator==(const iterator& other)
+			{
+				if (mNode->mData == other.mNode)
 				{
 					return true;
 				}
@@ -65,9 +70,9 @@ namespace MyDataStructure
 				return false;
 			}
 
-			bool operator !=(const iterator& other)
+			bool operator!=(const iterator& other)
 			{
-				if (mNode->mData != other->mNode)
+				if (mNode->mData != other.mNode->mData)
 				{
 					return true;
 				}
@@ -82,19 +87,20 @@ namespace MyDataStructure
 		List<T>& operator=(const List<T>& rhs);
 		~List();
 
+		iterator begin(void);
+		iterator end(void);
+
 		void push_front(T data);
 		void push_back(T data);
 		T pop_front(void);
 		T pop_back(void);
 
-		void insert(T data);
-		iterator erase(iterator it);
 		void clear(void);
 		size_t size(void) const;
 		bool empty(void) const;
 
-		iterator begin(void) const;
-		iterator end(void) const;
+		iterator erase(iterator it);
+		void remove(T data);
 
 	private:
 		size_t mSize;
@@ -102,5 +108,219 @@ namespace MyDataStructure
 		Node mTail;
 	};
 
+	template <typename T>
+	List<T>::List()
+		: mSize(0)
+	{
+		mHead.mPrev = nullptr;
+		mHead.mNext = &mTail;
 
+		mTail.mPrev = &mHead;
+		mTail.mNext = nullptr;
+	}
+
+	template <typename T>
+	List<T>::List(const List<T>& other)
+		: mSize(other.mSize)
+	{
+		mHead.mPrev = nullptr;
+		mHead.mNext = &mTail;
+
+		mTail.mPrev = &mHead;
+		mTail.mNext = nullptr;
+
+		for (iterator it = other.begin(); it != other.end(); ++it)
+		{
+			Node* node = new Node();
+			node->mData = *it;
+			node->mPrev = mTail.mPrev;
+			node->mNext = mTail;
+
+			mTail.mPrev->mNext = node;
+			mTail.mPrev = node;
+		}
+	}
+
+	template <typename T>
+	List<T>& List<T>::operator=(const List<T>& rhs)
+	{
+		if (this != &rhs)
+		{
+			mSize = rhs.mSize;
+
+			for (iterator it = begin(); it != end(); ++it)
+			{
+				delete (*it);
+			}
+			mHead.mNext = &mTail;
+			mTail.mPrev = &mHead;
+
+			for (iterator it = rhs.begin(); it != rhs.end(); ++it)
+			{
+				Node* node = new Node();
+				node->mData = *it;
+				node->mPrev = mTail.mPrev;
+				node->mNext = mTail;
+
+				mTail.mPrev->mNext = node;
+				mTail.mPrev = node;
+			}
+		}
+
+		return *this;
+	}
+
+	template <typename T>
+	List<T>::~List()
+	{
+		Node* cur = mHead.mNext;
+
+		for (int iCnt = 0; iCnt < mSize; iCnt++)
+		{
+			Node* next = cur->mNext;
+			delete cur;
+			cur = next;
+		}
+	}
+
+	template <typename T>
+	typename List<T>::iterator List<T>::begin(void)
+	{
+		List<T>::iterator it(mHead.mNext);
+		return it;
+	}
+
+	template <typename T>
+	typename List<T>::iterator List<T>::end(void)
+	{
+		List<T>::iterator it(&mTail);
+		return it;
+	}
+
+	template <typename T>
+	void List<T>::push_front(T data)
+	{
+		Node* node = new Node();
+		node->mData = data;
+
+		node->mNext = mHead.mNext;
+		mHead.mNext->mPrev = node;
+
+		node->mPrev = &mHead;
+		mHead.mNext = node;
+
+		mSize++;
+	}
+
+	template <typename T>
+	void List<T>::push_back(T data)
+	{
+		Node* node = new Node();
+		node->mData = data;
+
+		node->mPrev = mTail.mPrev;
+		mTail.mPrev->mNext = node;
+
+		node->mNext = &mTail;
+		mTail.mPrev = node;
+
+		mSize++;
+	}
+
+	template <typename T>
+	T List<T>::pop_front(void)
+	{
+		if (mSize == 0)
+		{
+			return;
+		}
+
+		Node* targetNode = mHead.mNext;
+		T returnValue = targetNode->mData;
+
+		mHead.mNext = targetNode->mNext;
+		targetNode->mNext->mPrev = &mHead;
+
+		delete targetNode;
+
+		mSize--;
+
+		return returnValue;
+	}
+
+	template <typename T>
+	T List<T>::pop_back(void)
+	{
+		if (mSize == 0)
+		{
+			return;
+		}
+
+		Node* targetNode = mTail.mPrev;
+		T returnValue = targetNode->mData;
+
+		mTail.mPrev = targetNode->mPrev;
+		targetNode->mPrev->mNext = &mTail;
+
+		delete targetNode;
+
+		mSize--;
+
+		return returnValue;
+	}
+
+	template <typename T>
+	void List<T>::clear(void)
+	{
+		Node* cur = mHead.mNext;
+
+		for (int iCnt = 0; iCnt < mSize; iCnt++)
+		{
+			Node* next = cur->mNext;
+			delete cur;
+			cur = next;
+		}
+
+		mSize = 0;
+		mHead.mNext = &mTail;
+		mTail.mPrev = &mHead;
+	}
+
+	template <typename T>
+	size_t List<T>::size(void) const
+	{
+		return mSize;
+	}
+
+	template <typename T>
+	bool List<T>::empty(void) const
+	{
+		return mHead->mNext == &mTail;
+	}
+
+	template <typename T>
+	typename List<T>::iterator List<T>::erase(iterator it)
+	{
+		Node* node = *it;
+		node->mPrev->mNext = node->mNext;
+		node->mNext->mPrev = node->mPrev;
+		
+		it.mNode = node->mNext;
+		delete node;
+
+		return it;
+	}
+
+	template <typename T>
+	void List<T>::remove(T data)
+	{
+		List<T>::iterator it;
+		for (it = begin(); it != end(); ++it)
+		{
+			if (*it == data)
+			{
+				erase(it);
+			}
+		}
+	}
 }
--- Assignment/03_06/Assginment_03_06/Assginment_03_06/main.cpp
+++ Assignment/03_06/Assginment_03_06/Assginment_03_06/main.cpp
@@ -1,8 +1,20 @@
+#include <cstdio>
 #include <vector>
+#include "List.h"
 
 int main(void)
 {
+	MyDataStructure::List<int> l;
+
+	l.push_back(1);
+	l.push_back(2);
+	l.push_back(3);
+	l.push_front(4);
+	l.push_front(5);
+
+	for (auto it = l.begin(); it != l.end(); ++it)
+	{
+		printf("%d ", *it);
+	}
 	std::vector<int> v;
-	v.begin();
-	return 0;
 }
