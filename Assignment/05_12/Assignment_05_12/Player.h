e988965245ce8eec812c8c79b83323cc7989cbc7
Kyeongmin Kim <kkfenix3085@naver.com>
2023-05-11 19:57:16 +0900

    Assignment_05_12 Add Macro Constants in Player.h

--- Assignment/05_12/Assignment_05_12/Player.h
+++ Assignment/05_12/Assignment_05_12/Player.h
@@ -11,6 +11,15 @@ constexpr int PLAYER_SPEED_Y = 2;
 #define RANGE_MOVE_RIGHT	630
 #define RANGE_MOVE_BOTTOM	470
 
+#define RANGE_MOVE_ERROR	50
+
+#define RANGE_ATTACK_1_X	80
+#define RANGE_ATTACK_2_X	90
+#define RANGE_ATTACK_3_X	100
+#define RANGE_ATTACK_1_Y	10
+#define RANGE_ATTACK_2_Y	10
+#define RANGE_ATTACK_3_Y	20
+
 using namespace MyDataStructure;
 
 typedef INT32 PlayerID;
