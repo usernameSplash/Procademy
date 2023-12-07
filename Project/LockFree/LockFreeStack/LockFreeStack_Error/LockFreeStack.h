52c4f1e2d4476ccea02ed153f89ee00ad7433f3d
Kyeongmin Kim <kkfenix3085@naver.com>
2023-12-07 17:00:17 +0900

    LockFreeQueue

---
+++ Project/LockFree/LockFreeStack/LockFreeStack_Error/LockFreeStack.h
@@ -0,0 +1,120 @@
+#pragma once
+
+template<typename T>
+class LockFreeStack
+{
+private:
+	struct Node
+	{
+		T _data;
+		Node* _next;
+	};
+
+public:
+	LockFreeStack()
+		: _top(nullptr)
+	{
+	}
+
+	~LockFreeStack()
+	{
+		Node* curNode = _top;
+		while (curNode != nullptr)
+		{
+			Node* deleteNode = curNode;
+			delete deleteNode;
+
+			curNode = curNode->_next;
+		}
+	}
+
+public:
+	void Push(T data)
+	{
+		Node* node = new Node;
+		node->_data = data;
+
+		Node* top;
+
+		do
+		{
+			top = _top;
+			node->_next = _top;
+		} while (!InterlockedCompareExchange64((LONG64*)&_top, (LONG64)node, (LONG64)top));
+	}
+
+	T Pop(void)
+	{
+		T data;
+		Node* top;
+		Node* next;
+
+		do
+		{
+			top = _top;
+			data = top->_data;
+			next = top->_next;
+		} while (!InterlockedCompareExchange64((LONG64*)&_top, (LONG64)next, (LONG64)top));
+
+		delete top;
+
+		return data;
+	}
+
+private:
+	Node* _top;
+
+	/*
+		For Debug
+	*/
+
+private:
+#define DEBUG_ARRAY_LEN 10000
+	struct DebugData
+	{
+		DebugData()
+			: _idx(-1)
+			, _threadId(-1)
+			, _operationCode(-1)
+			, _pNewTopNode(nullptr)
+			, _pOldTopNode(nullptr)
+			, _newTopVal(-1)
+			, _oldTopVal(-1)
+		{
+		}
+
+		void SetData(int idx, int threadId, int operationCode, Node* pNewTopNode, Node* pOldTopNode, T newTopVal, T oldTopVal)
+		{
+			_idx = idx;
+			_threadId = threadId;
+			_operationCode = operationCode;
+			_pNewTopNode = pNewTopNode;
+			_pOldTopNode = pOldTopNode;
+			_newTopVal = newTopVal;
+			_oldTopVal = oldTopVal;
+		}
+
+		int _idx;
+		int _threadId;
+		int _operationCode; //0 == Push, 1 == Pop;
+		Node* _pNewTopNode;
+		Node* _pOldTopNode;
+		T _newTopVal;
+		T _oldTopVal;
+	};
+
+private:
+	void LogData(int operationCode, Node* pNewTopNode, Node* pOldTopNode, T newTopVal, T oldTopVal)
+	{
+		long idx = InterlockedIncrement(&_idx);
+		int threadId = GetCurrentThreadId();
+
+		_debugArray[idx % DEBUG_ARRAY_LEN].SetData(idx, threadId, operationCode, pNewTopNode, pOldTopNode, newTopVal, oldTopVal);
+
+
+	}
+
+private:
+	long _debugIdx;
+	DebugData _debugArray[DEBUG_ARRAY_LEN];
+};
